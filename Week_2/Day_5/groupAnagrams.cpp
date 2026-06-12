9c255440832200f2cab36116ccf839817f384bcc
Vinay-singh8887 <vs592355@gmail.com>
2026-06-12 22:58:02 +0530

    Day_5 completed

---
+++ Week_2/Day_5/groupAnagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    unordered_map<string,vector<string>>mp;
+
+
+    void group(string s){
+        string temp =s;
+        sort(temp.begin(),temp.end());
+        mp[temp].push_back(s);
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    
+    for(auto&str:strs)
+     group(str);
+
+      vector<vector<string>>ans;
+
+     for(auto&[key,val]:mp)
+       ans.push_back(val);
+
+    return ans;
+        
+    }
+};

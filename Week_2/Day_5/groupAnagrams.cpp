296c439f1146ed98fa807190255498f274550c9e
enter <vs592355@gmail.com>
2026-06-14 21:21:26 +0530

    Save changes

--- Week_2/Day_5/groupAnagrams.cpp
+++ Week_2/Day_5/groupAnagrams.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
 public:
 
@@ -24,3 +25,31 @@ public:
         
     }
 };
+=======
+class Solution {
+public:
+
+    unordered_map<string,vector<string>>mp;
+
+
+    void group(string s){
+        string temp =s;
+        sort(temp.begin(),temp.end());
+        mp[temp].push_back(s);
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    
+    for(auto&str:strs)
+     group(str);
+
+      vector<vector<string>>ans;
+
+     for(auto&[key,val]:mp)
+       ans.push_back(val);
+
+    return ans;
+        
+    }
+};
+>>>>>>> c83cbd2 (Save changes)
